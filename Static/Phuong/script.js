a2b29ffe96d8c639703784cf4eb2cd650746e048
NTLuan21 <147013320+NTLuan21@users.noreply.github.com>
2023-11-11 17:02:42 +0700

    yes

--- Static/Phuong/script.js
+++ Static/Phuong/script.js
@@ -1,25 +1,24 @@
-const HCMlong = 106.660172;
-const HCMlat = 10.762622;
+const long = 106.660172;
+const lat = 10.762622;
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiaHV5azIxIiwiYSI6ImNsbnpzcWhycTEwbnYybWxsOTAydnc2YmYifQ.55__cADsvmLEm7G1pib5nA";
 
-function main() {
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: [HCMlong, HCMlat],
-    zoom: 15,
-  });
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [long, lat],
+  zoom: 15,
+});
 
-  map.addControl(
-    new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl,
-    })
-  );
+map.addControl(
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+  })
+);
 
-  let currentMarker; // This will keep track of the current marker on the map
+let currentMarker; // This will keep track of the current marker on the map
 
 // Add a click event to the map to perform reverse geocoding
 map.on("click", function (e) {
@@ -44,13 +43,12 @@ map.on("click", function (e) {
     })
     .then((data) => {
       if (data && data.features && data.features.length > 0) {
-        var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-          `<h6>Location Information: </h6>
-          <p>Address: ${data.features[0].place_name}</p>`
+        var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+          "Address: " + data.features[0].place_name
         );
         currentMarker.setPopup(popup).togglePopup(); // Set popup to marker and show it
       } else {
-        throw new Error("Unable to find the address of the location");
+        throw new Error("No address found for this location");
       }
     })
     .catch((error) => {
@@ -58,35 +56,59 @@ map.on("click", function (e) {
       currentMarker.remove(); // Remove the marker if geocoding fails.
     });
 });
+// Assuming the rest of your code before this...
 
-addControls(map)
+// 1. Add Fullscreen control
+map.addControl(new mapboxgl.FullscreenControl());
 
-grabAdData()
-  .catch((error) => {
-    console.log(`Error found: ${error.message}`);
+// 2. Add Zoom controls (Zoom in / Zoom out)
+map.addControl(new mapboxgl.NavigationControl());
+
+// 3. Add User Location control (this will show the user's location and allow for tracking)
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true, // Set to true to keep tracking user's location
+    showUserLocation: true, // Set to true to show user's location
   })
-  .then((data) => {
-    for (const ad of data.features) {
-      // Create a DOM element for each marker.
+);
+
+async function grabAdData() {
+  try {
+    const response = await fetch("/AdData.json");
+    const data = await response.json();
+
+    for (const feature of data.features) {
+      // Continue if the feature is not properly defined
+      if (!feature.properties) continue;
+
       const el = document.createElement("div");
-      //add if statement here to adjust color of icon
       el.className = "marker";
 
-      // Add markers to the map.
+      // Create the HTML content for the popup using the properties from the feature
+      const popupContent = `
+      <div>
+      <h3>Address: ${feature.properties.address}</h3>
+      <p>Quantity: ${feature.properties.quantity}</p>
+      <p>Area: ${feature.properties.area}</p>
+      <p>Land Type: ${feature.properties.landType}</p>
+      <p>Ad Format: ${feature.properties.adFormat}</p>
+      <p>Status: ${feature.properties.status}</p>
+      <p>Board Type: ${feature.properties.boardType}</p>
+      <p>Size: ${feature.properties.size}</p>
+    </div>
+      `;
+
+      const popup = new mapboxgl.Popup({ offset: 25 }).setHTML(popupContent);
+
+      // Tạo marker mới.
       const marker = new mapboxgl.Marker(el)
-        .setLngLat(ad.geometry.coordinates)
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `
-          <h6>${ad.properties.adFormat}</h6>
-          <p>${ad.properties.address}</p>
-          <p>${ad.properties.area}</p>
-          <p>${ad.properties.landType}</p>
-          <p style="font-weight: 900; font-style: italic">${ad.properties.status}</p>
-          `
-          )
-        )
+        .setLngLat(feature.geometry.coordinates)
+        .setPopup(popup)
         .addTo(map);
+
       // Add 'mouseenter' event listener to show the popup.
       el.addEventListener("mouseenter", () => {
         marker.getPopup().addTo(map);
@@ -96,17 +118,61 @@ grabAdData()
       el.addEventListener("mouseleave", () => {
         marker.getPopup().remove();
       });
+      // Add a click event listener for each marker
+      // When a marker is clicked, update and show the sidebar
       marker.getElement().addEventListener("click", function (event) {
         // Prevents the map click event (and thus the reverse geocoding) from firing
         event.stopPropagation();
 
         // Pass the properties of this specific feature to the sidebar
-        showSidebar(ad.properties);
+        showSidebar(feature.properties);
       });
     }
-  });
+  } catch (error) {
+    console.error(`Error fetching advertisement data: ${error.message}`);
+  }
+}
 
-  // Prevent clicks inside the sidebar from propagating to the map, which would hide the sidebar
+grabAdData();
+
+// Function to show sidebar with property information
+function showSidebar(properties) {
+  // Update the content of the sidebar
+  $("#infoContent").html(`
+    <h4>Address: ${properties.address}</h4>
+    <p>Quantity: ${properties.quantity}</p>
+    <p>Area: ${properties.area}</p>
+    <p>Land Type: ${properties.landType}</p>
+    <p>Ad Format: ${properties.adFormat}</p>
+    <p>Status: ${properties.status}</p>
+    <p>Board Type: ${properties.boardType}</p>
+    <p>Size: ${properties.size}</p>
+  `);
+
+  // Show the sidebar by adding the 'visible' class
+  $("#sidebar").addClass("visible");
+}
+// Function to hide the sidebar
+function hideSidebar() {
+  $("#sidebar").removeClass("visible");
+}
+// On map click, show sidebar with the location information
+map.on("click", function (e) {
+  // Perform a reverse geocode on the clicked location
+  var lngLat = e.lngLat;
+  var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
+
+  $.get(url, function (data) {
+    if (data.features.length > 0) {
+      var placeName = data.features[0].place_name;
+      showSidebar(`<strong>Location:</strong> ${placeName}`);
+    } else {
+      showSidebar("No location information found.");
+    }
+  });
+});
+
+// Prevent clicks inside the sidebar from propagating to the map, which would hide the sidebar
 $("#sidebar").click(function (event) {
   event.stopPropagation();
 });
@@ -114,11 +180,11 @@ $("#sidebar").click(function (event) {
 // Clicking on the map canvas (outside the sidebar) hides the sidebar
 $(map.getCanvas()).click(function (e) {
   if (!$(e.target).closest("#sidebar").length) {
-    hideSidebar(map);
+    hideSidebar();
   }
 }); // Event handler for the close button
 $("#closeSidebar").click(function () {
-  hideSidebar(map);
+  hideSidebar();
 });
 $(document).ready(function () {
   // On map click, center the map on the clicked location and show sidebar with information
@@ -141,6 +207,25 @@ $(document).ready(function () {
     });
   });
 });
+// Your existing map setup code
+
+// ...
+
+// Function to open the report modal
+function openReportModal() {
+  var overlay = document.getElementById("pageOverlay");
+  var reportModalElement = document.getElementById("reportModal");
+  var reportModal = new bootstrap.Modal(reportModalElement, {
+    backdrop: "static", // Keep static backdrop to prevent closing when clicking outside
+  });
+  reportModal.show();
+  overlay.style.display = "block"; // Show the overlay
+}
+
+function closeOverlay() {
+  var overlay = document.getElementById("pageOverlay");
+  overlay.style.display = "none"; // Hide the overlay
+}
 
 // Event listener for the 'Report Issue' button
 document
@@ -239,93 +324,3 @@ document.addEventListener("fullscreenchange", () => {
     overlay.classList.remove("active");
   }
 });
-
-}
-
-main();
-
-
-function addControls(map) {
-  // Assuming the rest of your code before this...
-
-  // 1. Add Fullscreen control
-  map.addControl(new mapboxgl.FullscreenControl());
-
-  // 2. Add Zoom controls (Zoom in / Zoom out)
-  map.addControl(new mapboxgl.NavigationControl());
-
-  // 3. Add User Location control (this will show the user's location and allow for tracking)
-  map.addControl(
-    new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      trackUserLocation: true, // Set to true to keep tracking user's location
-      showUserLocation: true, // Set to true to show user's location
-    })
-  );
-}
-
-async function grabAdData() {
-  try {
-    const data = await fetch("/AdData.json");
-    const jsonData = await data.json();
-    return jsonData;
-  } catch (error) {
-    throw new Error(`Error found: ${error.message}`);
-  }
-}
-
-// Function to show sidebar with property information
-function showSidebar(properties) {
-  // Update the content of the sidebar
-  $("#infoContent").html(`
-    <h4>Address: ${properties.address}</h4>
-    <p>Quantity: ${properties.quantity}</p>
-    <p>Area: ${properties.area}</p>
-    <p>Land Type: ${properties.landType}</p>
-    <p>Ad Format: ${properties.adFormat}</p>
-    <p>Status: ${properties.status}</p>
-    <p>Board Type: ${properties.boardType}</p>
-    <p>Size: ${properties.size}</p>
-  `);
-
-  // Show the sidebar by adding the 'visible' class
-  $("#sidebar").addClass("visible");
-}
-
-// Function to hide the sidebar
-function hideSidebar(map) {
-  $("#sidebar").removeClass("visible");
-}
-// On map click, show sidebar with the location information
-map.on("click", function (e) {
-  // Perform a reverse geocode on the clicked location
-  var lngLat = e.lngLat;
-  var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
-
-  $.get(url, function (data) {
-    if (data.features.length > 0) {
-      var placeName = data.features[0].place_name;
-      showSidebar(`<strong>Location:</strong> ${placeName}`);
-    } else {
-      showSidebar("No location information found.");
-    }
-  });
-});
-
-// Function to open the report modal
-function openReportModal() {
-  var overlay = document.getElementById("pageOverlay");
-  var reportModalElement = document.getElementById("reportModal");
-  var reportModal = new bootstrap.Modal(reportModalElement, {
-    backdrop: "static", // Keep static backdrop to prevent closing when clicking outside
-  });
-  reportModal.show();
-  overlay.style.display = "block"; // Show the overlay
-}
-
-function closeOverlay() {
-  var overlay = document.getElementById("pageOverlay");
-  overlay.style.display = "none"; // Hide the overlay
-}

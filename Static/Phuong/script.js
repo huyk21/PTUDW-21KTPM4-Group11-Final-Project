3128a8f9a4df99d54d6dc3d5ab8440a9f6f03632
DanielPham311 <phamquocduy3112003@gmail.com>
2023-11-13 21:36:19 +0700

    changes made for Phuong

--- Static/Phuong/script.js
+++ Static/Phuong/script.js
@@ -21,229 +21,223 @@ function main() {
 
   let currentMarker; // This will keep track of the current marker on the map
 
-// Add a click event to the map to perform reverse geocoding
-map.on("click", function (e) {
-  // Remove the previous marker if it exists
-  if (currentMarker) {
-    currentMarker.remove();
-  }
+  // Add a click event to the map to perform reverse geocoding
+  map.on("click", function (e) {
+    // Remove the previous marker if it exists
+    if (currentMarker) {
+      currentMarker.remove();
+    }
 
-  // Construct the URL for reverse geocoding using the clicked coordinates
-  var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng},${e.lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
+    // Construct the URL for reverse geocoding using the clicked coordinates
+    var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng},${e.lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
 
-  // Create a marker at the clicked location
-  currentMarker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map);
+    // Create a marker at the clicked location
+    currentMarker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map);
 
-  // Fetch the result
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
+    // Fetch the result
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data && data.features && data.features.length > 0) {
+          var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+            `<h6>Location Information: </h6>
+          <p>Address: ${data.features[0].place_name}</p>`
+          );
+          currentMarker.setPopup(popup).togglePopup(); // Set popup to marker and show it
+        } else {
+          throw new Error("Unable to find the address of the location");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching address: ", error);
+        currentMarker.remove(); // Remove the marker if geocoding fails.
+      });
+  });
+
+  addControls(map);
+
+  grabAdData()
+    .catch((error) => {
+      console.log(`Error found: ${error.message}`);
     })
     .then((data) => {
-      if (data && data.features && data.features.length > 0) {
-        var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
-          `<h6>Location Information: </h6>
-          <p>Address: ${data.features[0].place_name}</p>`
-        );
-        currentMarker.setPopup(popup).togglePopup(); // Set popup to marker and show it
-      } else {
-        throw new Error("Unable to find the address of the location");
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching address: ", error);
-      currentMarker.remove(); // Remove the marker if geocoding fails.
-    });
-});
+      for (const ad of data.features) {
+        // Create a DOM element for each marker.
+        const el = document.createElement("div");
+        //add if statement here to adjust color of icon
+        switch (ad.properties.status) {
+          case "ĐÃ QUY HOẠCH": {
+            el.className = "marker"
+            break
+          }
 
-addControls(map)
+          case "CHƯA QUY HOẠCH": {
+            el.className = "yellow"
+            break
+          }
 
-grabAdData()
-  .catch((error) => {
-    console.log(`Error found: ${error.message}`);
-  })
-  .then((data) => {
-    for (const ad of data.features) {
-      // Create a DOM element for each marker.
-      const el = document.createElement("div");
-      //add if statement here to adjust color of icon
-      el.className = "marker";
+          case "ĐÃ CẤP PHÉP": {
+            el.className = "green"
+            break
+          }
 
-      // Add markers to the map.
-      const marker = new mapboxgl.Marker(el)
-        .setLngLat(ad.geometry.coordinates)
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `
+          case "BỊ BÁO CÁO": {
+            el.className = "red"
+            break
+          }
+
+          default: {
+            el.className = "marker"
+          }
+        }
+
+        // Add markers to the map.
+        const marker = new mapboxgl.Marker(el)
+          .setLngLat(ad.geometry.coordinates)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }).setHTML(
+              `
           <h6>${ad.properties.adFormat}</h6>
           <p>${ad.properties.address}</p>
           <p>${ad.properties.area}</p>
           <p>${ad.properties.landType}</p>
           <p style="font-weight: 900; font-style: italic">${ad.properties.status}</p>
           `
+            )
           )
-        )
-        .addTo(map);
-      // Add 'mouseenter' event listener to show the popup.
-      el.addEventListener("mouseenter", () => {
-        marker.getPopup().addTo(map);
-      });
+          .addTo(map);
+        // Add 'mouseenter' event listener to show the popup.
+        el.addEventListener("mouseenter", () => {
+          marker.getPopup().addTo(map);
+        });
 
-      // Add 'mouseleave' event listener to remove the popup.
-      el.addEventListener("mouseleave", () => {
-        marker.getPopup().remove();
-      });
-      marker.getElement().addEventListener("click", function (event) {
-        // Prevents the map click event (and thus the reverse geocoding) from firing
-        event.stopPropagation();
+        // Add 'mouseleave' event listener to remove the popup.
+        el.addEventListener("mouseleave", () => {
+          marker.getPopup().remove();
+        });
+        marker.getElement().addEventListener("click", function (event) {
+          // Prevents the map click event (and thus the reverse geocoding) from firing
+          event.stopPropagation();
 
-        // Pass the properties of this specific feature to the sidebar
-        showSidebar(ad.properties);
-      });
-    }
-  });
-
-  // Prevent clicks inside the sidebar from propagating to the map, which would hide the sidebar
-$("#sidebar").click(function (event) {
-  event.stopPropagation();
-});
-
-// Clicking on the map canvas (outside the sidebar) hides the sidebar
-$(map.getCanvas()).click(function (e) {
-  if (!$(e.target).closest("#sidebar").length) {
-    hideSidebar(map);
-  }
-}); // Event handler for the close button
-$("#closeSidebar").click(function () {
-  hideSidebar(map);
-});
-$(document).ready(function () {
-  // On map click, center the map on the clicked location and show sidebar with information
-  map.on("click", function (e) {
-    var lngLat = e.lngLat;
-
-    // Center the map on the clicked location
-    map.flyTo({ center: lngLat });
-
-    // Perform a reverse geocode on the clicked location
-    var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
-
-    $.get(url, function (data) {
-      if (data.features.length > 0) {
-        var placeName = data.features[0].place_name;
-        showSidebar(`<strong>Location:</strong> ${placeName}`);
-      } else {
-        showSidebar("No location information found.");
+          // Pass the properties of this specific feature to the sidebar
+          showSidebar(ad.properties);
+        });
       }
     });
-  });
-});
 
-// Event listener for the 'Report Issue' button
-document
-  .getElementById("reportButton")
-  .addEventListener("click", openReportModal);
-
-// Event listener for closing the modal
-document
-  .getElementById("reportModal")
-  .addEventListener("hide.bs.modal", closeOverlay);
-
-// Event listener for the form submission
-document
-  .getElementById("reportForm")
-  .addEventListener("submit", function (event) {
-    closeOverlay(); // Hide the overlay
-    // Perform the submission logic or AJAX request here
-    // For example:
-    // $.post('/submit-report', $(this).serialize(), function(response) {
-    //   // Handle response
-    //   $('#reportModal').modal('hide'); // Hide the modal if the submission is successful
-    // });
+  // Prevent clicks inside the sidebar from propagating to the map, which would hide the sidebar
+  $("#sidebar").click(function (event) {
+    event.stopPropagation();
   });
 
-// Event listener for the 'Report Issue' button
-document
-  .getElementById("reportButton")
-  .addEventListener("click", openReportModal);
+  // Clicking on the map canvas (outside the sidebar) hides the sidebar
+  $(map.getCanvas()).click(function (e) {
+    if (!$(e.target).closest("#sidebar").length) {
+      hideSidebar(map);
+    }
+  }); // Event handler for the close button
+  $("#closeSidebar").click(function () {
+    hideSidebar(map);
+  });
+  $(document).ready(function () {
+    // On map click, center the map on the clicked location and show sidebar with information
+    map.on("click", function (e) {
+      var lngLat = e.lngLat;
 
-document.addEventListener("DOMContentLoaded", function () {
-  var overlay = document.getElementById("pageOverlay");
-  var toggleButton = document.getElementById("toggleSidebarButton");
-  var sidebar = document.getElementById("nav-sidebar");
+      // Center the map on the clicked location
+      map.flyTo({ center: lngLat });
 
-  toggleButton.addEventListener("click", function () {
-    // Hide the toggle button
-    // Show the sidebar
+      // Perform a reverse geocode on the clicked location
+      var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${lngLat.lng},${lngLat.lat}.json?access_token=${mapboxgl.accessToken}`;
+
+      $.get(url, function (data) {
+        if (data.features.length > 0) {
+          var placeName = data.features[0].place_name;
+          showSidebar(`<strong>Location:</strong> ${placeName}`);
+        } else {
+          showSidebar("No location information found.");
+        }
+      });
+    });
   });
 
-  // If you want to be able to close the sidebar, you'd add an event listener
-  // to a close button inside the sidebar. For example:
-  var closeButton = document.getElementById("closeSideBar");
-  closeButton.addEventListener("click", function () {});
-});
-document
-  .getElementById("toggleSidebarButton")
-  .addEventListener("click", function () {
+  document.addEventListener("DOMContentLoaded", function () {
+    var overlay = document.getElementById("pageOverlay");
+    var toggleButton = document.getElementById("toggleSidebarButton");
     var sidebar = document.getElementById("nav-sidebar");
+
+    toggleButton.addEventListener("click", function () {
+      // Hide the toggle button
+      // Show the sidebar
+    });
+
+    // If you want to be able to close the sidebar, you'd add an event listener
+    // to a close button inside the sidebar. For example:
+    var closeButton = document.getElementById("closeSideBar");
+    closeButton.addEventListener("click", function () {});
+  });
+  document
+    .getElementById("toggleSidebarButton")
+    .addEventListener("click", function () {
+      var sidebar = document.getElementById("nav-sidebar");
+    });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var sidebar = document.getElementById("nav-sidebar");
+    var toggleSidebarButton = document.getElementById("toggleSidebarButton");
+    var closeSideBarButton = document.getElementById("closeSideBar");
+    var pageOverlay = document.getElementById("pageOverlay");
+    var navSidebar = document.getElementById("nav-sidebar");
+
+    // Function to open the sidebar
+    function openSidebar() {
+      navSidebar.style.transform = "translateX(0%)";
+      navSidebar.style.transition = "transform 0.5s";
+      pageOverlay.style.display = "block";
+    }
+
+    // Function to close the sidebar
+    function closeSidebar() {
+      navSidebar.style.transform = "translateX(-100%)";
+      navSidebar.style.transition = "transform 0.5s";
+      pageOverlay.style.display = "none";
+    }
+
+    // Event listener to toggle the sidebar
+    toggleSidebarButton.addEventListener("click", openSidebar);
+
+    // Event listener for closing sidebar using the close button
+    closeSideBarButton.addEventListener("click", closeSidebar);
+
+    // Event listener to close the sidebar when clicking on the overlay
+    pageOverlay.addEventListener("click", function () {
+      closeSidebar();
+    });
   });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var sidebar = document.getElementById("nav-sidebar");
-  var toggleSidebarButton = document.getElementById("toggleSidebarButton");
-  var closeSideBarButton = document.getElementById("closeSideBar");
-  var pageOverlay = document.getElementById("pageOverlay");
-  var navSidebar = document.getElementById("nav-sidebar");
+  document.addEventListener("fullscreenchange", () => {
+    var overlay = document.getElementById("pageOverlay");
+    var fsElement = document.fullscreenElement;
 
-  // Function to open the sidebar
-  function openSidebar() {
-    navSidebar.style.transform = "translateX(0%)";
-    navSidebar.style.transition = "transform 0.5s";
-    pageOverlay.style.display = "block";
-  }
-
-  // Function to close the sidebar
-  function closeSidebar() {
-    navSidebar.style.transform = "translateX(-100%)";
-    navSidebar.style.transition = "transform 0.5s";
-    pageOverlay.style.display = "none";
-  }
-
-  // Event listener to toggle the sidebar
-  toggleSidebarButton.addEventListener("click", openSidebar);
-
-  // Event listener for closing sidebar using the close button
-  closeSideBarButton.addEventListener("click", closeSidebar);
-
-  // Event listener to close the sidebar when clicking on the overlay
-  pageOverlay.addEventListener("click", function () {
-    closeSidebar();
+    if (fsElement) {
+      // Append the overlay to the fullscreen element, which is the Mapbox container
+      fsElement.appendChild(overlay);
+      overlay.style.zIndex = "500"; // Ensure it's on top
+      overlay.classList.add("active"); // Use 'active' class to control visibility with opacity
+    } else {
+      // If exiting fullscreen, hide the overlay and move it back to its original place
+      document.body.appendChild(overlay); // Or to wherever it originally is
+      overlay.classList.remove("active");
+    }
   });
-});
-
-document.addEventListener("fullscreenchange", () => {
-  var overlay = document.getElementById("pageOverlay");
-  var fsElement = document.fullscreenElement;
-
-  if (fsElement) {
-    // Append the overlay to the fullscreen element, which is the Mapbox container
-    fsElement.appendChild(overlay);
-    overlay.style.zIndex = "500"; // Ensure it's on top
-    overlay.classList.add("active"); // Use 'active' class to control visibility with opacity
-  } else {
-    // If exiting fullscreen, hide the overlay and move it back to its original place
-    document.body.appendChild(overlay); // Or to wherever it originally is
-    overlay.classList.remove("active");
-  }
-});
-
 }
 
 main();
-
 
 function addControls(map) {
   // Assuming the rest of your code before this...

7ca38eb88ba17594b25573b3c619fc9b4f1b7ab5
huyk21 <92777449+huyk21@users.noreply.github.com>
2023-11-30 19:03:02 +0700

    change dynamic, static to backend, frontend connect to mongodb atlas update package.json update .env add necessary packages

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 dotenv.config();
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+
 const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

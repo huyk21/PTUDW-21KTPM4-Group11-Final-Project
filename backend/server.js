6b8f27a24e5c15d73ed2434f7a99201051c6c743
huyk21 <92777449+huyk21@users.noreply.github.com>
2023-12-07 15:08:46 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import PhuongRoutes from "./routes/PhuongRoutes.js";
+import NguoiDanRoutes from "./routes/NguoiDanRoutes.js";
 const __dirname = path.dirname(new URL(import.meta.url).pathname).substring(1);
 
 const port = process.env.PORT || 4000;
@@ -34,6 +36,7 @@ app.engine(
 );
 
 app.set("view engine", "hbs"); // set view engine
+app.use("/api/nguoidan", NguoiDanRoutes);
 app.get("/", (req, res) => {
   res.render("index", { layout: "layout" });
 });

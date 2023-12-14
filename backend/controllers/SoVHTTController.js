fd56f307703600a122d19d3da8686435c452312c
Giant09dev <aidimi2810.sgn@gmail.com>
2023-12-14 17:09:14 +0700

    import models và chạy thử, sửa script trong package.json, sửa MONGO_URI trong .env

--- backend/controllers/SoVHTTController.js
+++ backend/controllers/SoVHTTController.js
@@ -3,8 +3,48 @@ import asyncHandler from "../middleware/asyncHandler.js";
 //@route POST /api/users
 //@access Public
 
+import User from "../models/UserModel.js";
+import Location from "../models/LocationModel.js";
+import AdBoard from "../models/AdBoardModel.js";
+import AdjustBoard from "../models/AdjustBoardModel.js";
+import AdjustLocation from "../models/AdjustLocationModel.js";
+import District from "../models/DistrictModel.js";
+import LicenseRequest from "../models/LicenseRequest.js";
+import Report from "../models/ReportModel.js";
+import ReportSolution from "../models/ReportSolutionModel.js";
+import Ward from "../models/WardModel.js";
+
 const index = asyncHandler(async (req, res) => {
-  res.send("trang chu ne!!!");
+  const Users = await User.find({});
+
+  const Locations = await Location.find({});
+
+  const AdBoards = await AdBoard.find({});
+
+  const AdjustBoards = await AdjustBoard.find({});
+
+  const AdjustLocations = await AdjustLocation.find({});
+
+  const Districts = await District.find({});
+
+  const LicenseRequests = await LicenseRequest.find({});
+
+  const Reports = await Report.find({});
+
+  const ReportSolutions = await ReportSolution.find({});
+
+  const Wards = await Ward.find({});
+  res.json({
+    Users,
+    Locations,
+    AdBoards,
+    AdjustBoards,
+    AdjustLocations,
+    Districts,
+    LicenseRequests,
+    Reports,
+    ReportSolutions,
+  });
 });
 
 const danhSachQuan = asyncHandler(async (req, res) => {

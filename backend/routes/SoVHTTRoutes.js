c487657a495ea52a5630a7655975ae6456681b4a
Giant09dev <aidimi2810.sgn@gmail.com>
2023-12-21 16:18:12 +0700

    adw

--- backend/routes/SoVHTTRoutes.js
+++ backend/routes/SoVHTTRoutes.js
@@ -17,8 +17,11 @@ import {
   danhSachHinhThucQuangCao,
 } from "../controllers/SoVHTTController.js";
 
+import { authUser, logoutUser } from "../controllers/UserController.js";
+import { protect, sovhtt } from "../middleware/authMiddleware.js";
+
 router.route("/").post(index).get(index);
-router.route("/them-diem-dat").post(addLocation);
+router.route("/them-diem-dat").post(protect, sovhtt, addLocation);
 router.route("/sua-diem-dat/:id").post(updateLocation);
 router.route("/xoa-diem-dat/:id").delete(deleteLocation);
 // router.route("/database").post(dbGenerator).get(dbGenerator);

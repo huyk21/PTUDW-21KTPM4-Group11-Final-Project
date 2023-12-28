8d945246c574a2af92d02e9fcdd178df0d2600a7
huyk21 <92777449+huyk21@users.noreply.github.com>
2023-12-28 14:58:27 +0700

    update phan upload image

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,20 @@ app.use("/api/", UserRoutes);
 app.get("/", (req, res) => {
   res.render("index", { layout: "layout2" });
 });
+if (process.env.NODE_ENV === "production") {
+  // const __dirname = path.resolve();
+  // app.use('/uploads', express.static('/var/data/uploads'));
+  // app.use(express.static(path.join(__dirname, '/frontend/build')));
+  // app.get('*', (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  // );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port, () => {

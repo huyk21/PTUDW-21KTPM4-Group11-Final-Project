6d34381acf972d8e5cd4ed3a65ba44386061eb51
NTLuan21 <147013320+NTLuan21@users.noreply.github.com>
2024-01-07 09:52:05 +0700

    captcha oke

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -7,7 +7,7 @@ import {
 import { protect, captcha } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/auth").post(captcha, authUser);
-
+router.route("/auth").post(authUser).get(showLogin);
+router.route("/captcha").post(captcha);
 router.route("/logout").post(logoutUser).get(authUser);
 export default router;

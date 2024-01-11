18f398e9f19b18a23125f9ce41f006cba38c6084
DanielPham311 <phamquocduy3112003@gmail.com>
2024-01-11 15:34:08 +0700

    added reset account password

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,9 @@ import generateToken from "../utils/generateToken.js";
 import User from "../models/UserModel.js";
 import Location from "../models/LocationModel.js";
 import session from "express-session";
+import Swal from "sweetalert2"
+import MailService from "../html/assets/js/emailService.js";
+import bcrypt from "bcryptjs"
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
@@ -50,7 +53,73 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-const logoutUser = (req, res) => {
+function generateOTP() {
+  // Create an empty string to store the OTP
+  let otp = "";
+
+  // Loop 6 times to generate 6 digits
+  for (let i = 0; i < 6; i++) {
+    // Generate a random number between 0 and 9 using Math.random()
+    let randomDigit = Math.floor(Math.random() * 10);
+
+    // Add the random digit to the OTP string
+    otp += randomDigit;
+  }
+
+  // Return the 6-digit OTP
+  return otp;
+}
+
+const showChangePassword = asyncHandler(async(req, res) => {
+  res.render("changePassword", {layout: "layoutLogin"})
+}) 
+
+const requestOTP = asyncHandler(async(req, res) => {
+  const email = req.body.email
+  const pw = req.body.password
+  const pwConfirm = req.body.passwordConfirm
+
+  if (pw !== pwConfirm) {
+    res.redirect("/api/changePassword?success=false")
+  }
+
+  else {
+    const OTP = generateOTP()
+    MailService.sendOTP(email, OTP)
+    req.session.OTP = OTP
+    req.session.userEmail = email
+    req.session.password = pw
+    res.render("confirmOTP", {layout: "layoutLogin"})
+  }
+})
+
+const showConfirmOTP =  asyncHandler(async(req, res) => {
+  res.render("confirmOTP", {layout: "layoutLogin"})
+})
+
+const confirmOTP = asyncHandler(async(req, res) => {
+  const generatedOTP = req.session.OTP
+  const OTP = req.body.otp
+  const password = req.session.password
+  const email = req.session.userEmail
+
+  if (OTP !== generatedOTP) {
+    res.redirect("/api/changePassword/confirmOTP?confirm=false")
+  }
+
+  else {
+    const user = await User.findOne({email: email})
+
+    await user.updateOne({password: bcrypt.hashSync(password, 10)})
+    res.redirect("/api/auth?reset=true")
+  }
+})
+
+const changePassword = asyncHandler(async(req, res) => {
+
+}) 
+
+const logoutUser = asyncHandler(async(req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.log(err);
@@ -65,7 +134,8 @@ const logoutUser = (req, res) => {
     expires: new Date(0),
   });
   res.status(200).json({ message: "Logged out successfully" });
-};
+});
+
 const showPlace = asyncHandler(async (req, res) => {
   const location = await Location.findById(req.params.id).populate(
     "district",
@@ -74,4 +144,4 @@ const showPlace = asyncHandler(async (req, res) => {
 
   res.redirect("/");
 });
-export { showPlace, authUser, logoutUser, showLogin };
+export { showPlace, authUser, logoutUser, showLogin, showChangePassword, requestOTP, changePassword, confirmOTP, showConfirmOTP };
